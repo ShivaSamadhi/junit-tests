e861d60b3e2ea3e46f77e6ba627c9cd965a4e179
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-24 16:21:31 -0500

    incomplete exercise

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -1,11 +1,18 @@
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CohortTest extends TestCase {
 
     public void testGetCohortAverage() {
     }
 
     public void testAddStudent() {
+        List<Student> students = new ArrayList<>();
+       Student student = new Student(1, "Ramaj");
+        Cohort.addStudent(student);
+        assertTrue(students.contains("Ramaj"));
     }
 
     public void testGetStudents() {
